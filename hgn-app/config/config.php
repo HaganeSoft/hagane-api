29577640e72609969064ea2fe9fef97f096b79db
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-21 16:02:18 -0600

    mensjaes de eeror y api key en config

--- hgn-app/config/config.php
+++ hgn-app/config/config.php
@@ -20,7 +20,7 @@ class Config {
 				'db_user' => 'root',
 				'db_password' => '',
 				'session_time' => 3600,
-				'conectaApiKey' => 'key_eYvWV7gSDkNYXsmr',
+				'conecta_api_key' => 'key_eYvWV7gSDkNYXsmr',
 				'document_root' => '/'
 			);
 	}
